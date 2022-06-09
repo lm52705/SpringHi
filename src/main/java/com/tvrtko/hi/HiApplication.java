128cea4ce12ab37bd550fe0d314baf292a71a680
lm52705 <lucija.marincic@fer.hr>
2022-06-10 00:21:49 +0200

    Controller changes

--- src/main/java/com/tvrtko/hi/HiApplication.java
+++ src/main/java/com/tvrtko/hi/HiApplication.java
@@ -2,8 +2,12 @@ package com.tvrtko.hi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.tvrtko.hi.controllers.HiController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses=HiController.class)
 public class HiApplication {
 
 	public static void main(String[] args) {
