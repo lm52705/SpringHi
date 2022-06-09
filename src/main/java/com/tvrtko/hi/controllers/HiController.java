c26bca4720deb98be5f365313ff2686d427fb9ec
lm52705 <lucija.marincic@fer.hr>
2022-06-10 00:12:04 +0200

    Changed the implementation in a way it can take query parameter name

--- src/main/java/com/tvrtko/hi/controllers/HiController.java
+++ src/main/java/com/tvrtko/hi/controllers/HiController.java
@@ -1,14 +1,17 @@
 package com.tvrtko.hi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HiController {
     
-    @GetMapping(value = "/hi")
-    public String SayHi() {
-        return ("Hi man/woman!");
+	@GetMapping(value = "/hey")
+    @ResponseBody
+    public String SayHi(@RequestParam(defaultValue = "man/woman") String name) {
+        return ("Hi " + name + "!");
     }
 
 }
